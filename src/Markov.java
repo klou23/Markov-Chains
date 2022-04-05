0178357d38becdd0190b961613a4dd55934230da
Kevin Lou <58053969+klou23@users.noreply.github.com>
2022-04-04 23:40:16 -0600

    added header and comments

--- src/Markov.java
+++ src/Markov.java
@@ -1,3 +1,10 @@
+/**
+ * Markov.java
+ * Kevin Lou
+ * 2022/04/04
+ * An implementation of text-generation using markov chains
+ */
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -5,13 +12,13 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Markov {
 
     public static void main(String[] args) throws IOException {
+        //read input file
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Input file name: ");
         String inputFile = consoleReader.readLine();
@@ -19,14 +26,17 @@ public class Markov {
         List<String> sentenceStart = new ArrayList<>();
         HashMap<String, List<String>> after = new HashMap<>();
 
+        //read in sample text
         BufferedReader br = new BufferedReader(new FileReader(inputFile));
         StringBuilder sb = new StringBuilder();
         String line = "";
         while((line = br.readLine()) != null){
             sb.append(line);
         }
+        //split into sentences
         StringTokenizer st1 = new StringTokenizer(sb.toString(), ".;?!");
         while(st1.hasMoreTokens()){
+            //split by words and put into hashmap
             StringTokenizer st2 = new StringTokenizer(st1.nextToken(), " ");
             String prev = st2.nextToken().replaceAll("[^A-Za-z'-]", "");
             while(prev.length() == 0 && st2.hasMoreTokens()) prev = st2.nextToken().replaceAll("[^A-Za-z'-]", "");
@@ -43,11 +53,13 @@ public class Markov {
             after.get(prev).add(".");
         }
 
+        //request word count and output file
         System.out.print("Number of words: ");
         int N = Integer.parseInt(consoleReader.readLine());
         System.out.print("Output file name: ");
         String outputFile = consoleReader.readLine();
 
+        //generate output and append into string builder
         StringBuilder sol = new StringBuilder();
         String last = ".";
         while(N-- > 0){
@@ -61,11 +73,16 @@ public class Markov {
             sol.append(add).append(" ");
             last = add;
         }
+
+        //print out final output
         PrintWriter pw = new PrintWriter(outputFile);
         pw.print(sol);
         pw.close();
     }
 
+    /**
+     * Utility method to get a random string from a list
+     */
     public static String getRand(List<String> list){
         return list.get((int) (Math.random()*list.size()));
     }
